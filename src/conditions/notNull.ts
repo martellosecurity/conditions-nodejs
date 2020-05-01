d6d3bd0a52841b92d8906fce57035bff67bd246f
Garrett Heaver <garrett@iterationfour.com>
2020-05-01 20:06:41 +0100

    adding tsdoc to notNull condition

--- src/conditions/notNull.ts
+++ src/conditions/notNull.ts
@@ -1,6 +1,14 @@
 class NullValueError extends Error {
 }
 
+/**
+ * Condition asserts that value is not null or undefined.
+ *
+ * @param value - Required value on which to run the condition.
+ * @param message - Optional custom message to be thrown on failure.
+ * @returns Original value it it meets the condition.
+ * @throws NullValueError if value fails to meet the condition.
+ */
 function notNull<T>(value: T, message = 'value must not be null'): T {
 
   // strict check on both values covered by condition
